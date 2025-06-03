de3173dd0f9de351eff7f129bdec3438477346d8
nesteagle <104323706+nesteagle@users.noreply.github.com>
2025-06-02 18:24:52 -0700

    Remove unused files and refactored templates for better clarity, structure

--- app.py
+++ app.py
@@ -1,47 +1,29 @@
 import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import (Flask, render_template)
 
 app = Flask(__name__)
-
+PROJECTS = ["general", "games", "web"]
 
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return render_template('index.html', projects=PROJECTS)
 
 @app.route('/general')
 def project():
    print('Request for project page received')
-   return render_template('projects/general.html')
+   return render_template('projects/general.html', projects=PROJECTS, selected_project_name = 'general')
 
 @app.route('/games')
 def games():
    print('Request for games page received')
-   return render_template('projects/games.html')
+   return render_template('projects/games.html', projects=PROJECTS, selected_project_name = 'games')
 
 @app.route('/web')
 def web():
    print('Request for web page received')
-   return render_template('projects/web.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+   return render_template('projects/web.html', projects=PROJECTS, selected_project_name = 'web')
 
 if __name__ == '__main__':
    app.run()
